cc26ca8475e099662554d2d1e642d01224aa0871
EmiratD <lamzar031408@gmail.com>
2023-01-16 05:42:08 +0600

    item card

--- src/components/itemCard/ItemCard.tsx
+++ src/components/itemCard/ItemCard.tsx
@@ -1,9 +1,27 @@
+import {Basket, Heart} from '../../assets/svg/ItemCardSvg';
 
+interface ItemInfo{
+  name: string;
+  price: number;
+  imge: string;
+}
 
-const ItemCard = () => {
+const ItemCard: ({}:ItemInfo) => JSX.Element  = ({ name, price, imge }) => {
   return (
-    <div>
-      
+    <div className="itemCard">
+      <div className="itemCard-img">
+        <img src={imge} alt="" />
+      </div>
+      <div className="itemCard-info">
+        <p className="itemCard-info__name">{name}</p>
+        <p className="itemCard-info__price">{price}<span className='dollar'>$</span></p>
+      </div>
+      <div className="itemCard-heart">
+        <Heart/>
+      </div>
+      <div className="itemCard-basket">
+        <Basket/>
+      </div>
     </div>
   );
 };
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,30 @@
 import  AboutUsBord from "../components/aboutUs/AboutUs";
 import SignInAndLogIn from "../components/signInAndLogIn/SignInAndLogIn";
+import ItemCard from "../components/itemCard/ItemCard";
 
 const HomePage = () => {
+
+  const item = <ItemCard name = {'nike'} price = {54} imge = {'https://e7.pngegg.com/pngimages/507/572/png-clipart-sneakers-sneaker-collecting-shoe-vans-adidas-adidas-white-text.png'}/>;
+
+  const itemArr = [
+    item,
+    item,
+    item,
+    item,
+    item,
+    item,
+    item,
+    item,
+  ];
+
   return(
       <main className="wrapper">
         {AboutUsBord}
         <div className="m0auto">
           <SignInAndLogIn/>
+        </div>
+        <div className="bord">
+          {itemArr.map(item => item)}
         </div>
       </main>
   )
