612a5f3146c9ef1637d18990e04b3178ea3cd854
EmiratD <lamzar031408@gmail.com>
2023-01-18 05:45:47 +0600

    brand slider mode style

--- src/components/sliderLogo/SliderLogo.tsx
+++ src/components/sliderLogo/SliderLogo.tsx
@@ -1,18 +1,17 @@
-import logo from "../../assets/svg/brandIcon";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
+
+// import required modules
+import { Autoplay } from "swiper";
+
+import logo from "../../assets/svg/brandIcon";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-// import "./styles.css";
-
-// import required modules
-import { Autoplay } from "swiper";
-
-export default () => {
+const SliderLogo = () => {
   return (
     <>
       <Swiper
@@ -20,33 +19,17 @@ export default () => {
         spaceBetween={10}
         loop={true}
         autoplay={{
-            delay: 1500,
-            disableOnInteraction: false
+          delay: 1500,
+          disableOnInteraction: false,
         }}
-        modules={[Autoplay,]}
+        modules={[Autoplay]}
       >
-        {logo.map(logoBrand => <SwiperSlide>{logoBrand}</SwiperSlide>)}
-
+        {logo.map((logoBrand) => (
+          <SwiperSlide>{logoBrand}</SwiperSlide>
+        ))}
       </Swiper>
     </>
   );
-}
-// import { Virtual } from 'swiper';
-// import { Swiper, SwiperSlide } from 'swiper/react';
+};
 
-// // Import Swiper styles
-// import 'swiper/css';
-// import 'swiper/css/virtual';
-
-// export default () => {
-
-//   return (
-//     <Swiper modules={[Virtual]} spaceBetween={10} slidesPerView={6} loop={true} virtual>
-//       {logo.map((slideContent, index) => (
-//         <SwiperSlide key={`logo${index}`} virtualIndex={index}>
-//           {slideContent}
-//         </SwiperSlide>
-//       ))}
-//     </Swiper>
-//   );
-// };
+export default SliderLogo;
